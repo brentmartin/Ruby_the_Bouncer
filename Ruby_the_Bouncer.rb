6dd511703c84ab418e105c2d73d3372be8b24535
Brent Martin <bmart35@gmail.com>
2016-03-08 07:31:06 -0600

    added exit and some refactoring

--- Ruby_the_Bouncer.rb
+++ Ruby_the_Bouncer.rb
@@ -5,6 +5,13 @@ def confused_waiting
   end
 end
 
+def just_working
+  3.times do
+    sleep 1
+    puts "workin'"
+  end
+end
+
 def intro
   puts "You there!"
   print ">  "
@@ -22,9 +29,6 @@ def intro
   sleep 0.5
   puts ", and I'm the ruby bouncer!"
   sleep 0.5
-  puts "How old are you?"
-  sleep 0.1
-  print ">  "
 end
 
 def under_17
@@ -38,7 +42,7 @@ def under_17
   sleep 1.5
   print "Now beat it kid! "
   sleep 0.5
-  print "I've got things to do."
+  puts "I've got things to do."
 end
 
 def over_18
@@ -46,9 +50,9 @@ def over_18
   sleep 1
   puts "Looks like you can vote and smoke cigs."
   sleep 1.5
-  puts "Now if you'll excuse me. "
+  print "Now if you'll excuse me. "
   sleep 0.5
-  print "I've got things to do."
+  puts "I've got things to do."
 end
 
 def over_21
@@ -60,9 +64,9 @@ def over_21
   sleep 0.5
   puts "it looks like you can vote and smoke cigs."
   sleep 1.5
-  puts "Now if you'll excuse me. "
+  print "Now if you'll excuse me. "
   sleep 0.5
-  print "I've got things to do."
+  puts "I've got things to do."
 end
 
 def over_25
@@ -80,22 +84,75 @@ def over_25
   sleep 1.5
   print "Now if you'll excuse me. "
   sleep 0.5
-  print "I've got things to do."
+  puts "I've got things to do."
 end
 
-intro
-
-answer = gets.chomp.to_i
-puts "So you are #{answer} years old?"
-sleep 1
-if answer <= 17
-  under_17
-elsif answer >= 18 && answer <= 20
-  over_18
-elsif answer >= 21 && answer <= 24
-  over_21
-elsif answer >= 25
-  over_25
-elsif
-  puts "You ain't making sense."
+def ask_again
+  sleep 1
+  print "Just "
+  just_working
+  print "I'ma "
+  just_working
+  print "wooor"
+  confused_waiting
+  puts ""
+  puts "* falls asleep *"
+  sleep 2
+  puts "* hours pass *"
+  sleep 2
+  puts "* awakens *"
+  sleep 0.5
+  print "huh?"
+  confused_waiting
+  print "what?"
+  confused_waiting
+  puts "Hey!"
+  sleep 0.5
+  puts "I'm the ruby bouncer!"
+  sleep 0.5
 end
+
+def here_for
+  puts "Are you here for the things?"
+  puts "You can (Q)uit or (P)arty"
+  print ">  "
+  for_things = gets.chomp
+  if for_things.upcase == "Q"
+    puts "You have a nice day now."
+    exit
+  end
+end
+
+def how_old
+  puts "How old are you?"
+  sleep 0.1
+  print ">  "
+end
+
+def check_age
+  intro
+  loop do
+    here_for
+    how_old
+    answer = gets.chomp.to_i
+    puts "So you are #{answer} years old?"
+    sleep 1
+    if answer <= 17
+      under_17
+      ask_again
+    elsif answer >= 18 && answer <= 20
+      over_18
+      ask_again
+    elsif answer >= 21 && answer <= 24
+      over_21
+      ask_again
+    elsif answer >= 25
+      over_25
+      ask_again
+    elsif
+      puts "You ain't making sense."
+    end
+  end
+end
+
+check_age
